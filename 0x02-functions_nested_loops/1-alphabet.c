77bac3a948ca26508f6502d88226d445d2f90bb6
Ursull Abomo <amurielleursull@gmail.com>
2022-06-07 16:17:20 -0700

    Prints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints alphabets
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+
+	return (0);
+}
