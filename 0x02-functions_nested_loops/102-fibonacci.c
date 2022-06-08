9c800d6d0bdb2ae24c82114172b71219609bc979
Ursull Abomo <amurielleursull@gmail.com>
2022-06-08 01:08:34 -0700

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,12 +12,12 @@ int main(void)
 	j = 1;
 	k = 2;
 
-	for (i =1; i <= 50; ++i)
+	for (i = 1; i <= 50; ++i)
 	{
 		if (j != 20365011074)
 		{
 			printf("%ld, ", j);
-		} else 
+		}else 
 		{
 			printf("%ld\n", j);
 		}
