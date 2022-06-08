51f1628c642e3ec2649f65e7d31893a0d5fe44c7
Ursull Abomo <amurielleursull@gmail.com>
2022-06-08 00:34:07 -0700

    Natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,7 +5,7 @@
  * of 3 or 5 below 1024
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	unsigned long int sum3, sum5, sum;
 	int i;
@@ -14,14 +14,14 @@ int main (void)
 	sum5 = 0;
 	sum = 0;
 
-	for (i =0; i < 1024; ++i)
+	for (i = 0; i < 1024; ++i)
 	{
 		if ((i % 3) == 0)
 		{
 			sum3 = sum3 + i;
 		} else if ((i % 5) == 0)
 		{
-			sum5 = sum5 +i;
+			sum5 = sum5 + i;
 		}
 	}
 	sum = sum3 + sum5;
