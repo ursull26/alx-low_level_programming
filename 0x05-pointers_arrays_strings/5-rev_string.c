d27ddfd7a759993913bdf4d23fbb1f73bc9a406a
Ursull Abomo <amurielleursull@gmail.com>
2022-06-13 14:56:19 -0700

    Rverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ * @str: string to be reserved.
+ */
+void rev_string(char *str)
+{
+	int i = 0, x = 0;
+	char tmp;
+
+	while (str[i++])
+		x++;
+
+	for (i = x - 1; i >= x / 2; i--)
+	{
+		tmp = s[i];
+		str[i] = str[x - i - 1];
+		str[x - i - 1] = tmp;
+	}
+}
