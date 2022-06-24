131fad3ea5327623b3fbd0b8a942c28ad21e13ba
Ursull Abomo <amurielleursull@gmail.com>
2022-06-24 11:18:10 -0700

    Prints the addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - Prints the addition of positive numbers, 
+ * main - Prints the addition of positive numbers,
  * followed by a new line
  * @argc: The number of arguments passed to the program
  * @argv: An array of pointers to the arguments
