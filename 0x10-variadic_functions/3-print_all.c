2e64aa749071c658780e5ede213522c782aadbad
Ursull Abomo <amurielleursull@gmail.com>
2022-07-07 11:37:39 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 	char *str;
 	const char t_arg[] = "cifs";
 
-	ca_start(valist, format);
+	va_start(valist, format);
 	while (format && format[i])
 	{
 		j = 0;
