2157bb1d83d5dff2bf4a38d4ed0726ed0f20790a
ursull <amurielleursull@gmail.com>
2022-07-19 07:36:08 -0700

    addnode

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,7 +4,7 @@
 /**
  * listint_len - Returns the number of elements
  * in a linked listint_t list.
- * @h: A pointer to the head of the listint_t list
+ * @h: A pointer to the head
  */
 size_t listint_len(const listint_t *h)
 {
